242909abc82c8f8db870d5b901979343dce6408c
marcospepin <marcosksgomez@gmail.com>
2025-12-10 13:37:38 +0100

    all work optimizado

--- test.php
+++
@@ -1,8 +0,0 @@
-﻿<?php
-header('Content-Type: application/json');
-echo json_encode([
-    'status' => 'ok',
-    'php_version' => phpversion(),
-    'mysql_available' => extension_loaded('mysqli')
-]);
-?>
